cbbed50a99a00a2d1faaa691f368c9b93f41c3c4
joseph <joseph@mc-server.local>
2024-11-04 20:41:24 -0500

    fixed the livestream stuff, not works to get the stream

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,11 @@
 <style global>
   @import "../app.css";
 </style>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<link href="https://vjs.zencdn.net/7.17.0/video-js.css" rel="stylesheet" />
-<video
-  id="live-stream"
-  class="video-js vjs-default-skin"
-  controls
-  preload="auto"
-  data-setup="{'autoplay'}"
->
-  <source
-    src="https://stream.josephheinz.live/hls/zebra.m3u8"
-    type="application/vnd.apple.mpegurl"
-  >
-</video>
-<script src="https://vjs.zencdn.net/7.17.0/video.min.js"></script>
+<script>
+    import VideoPlayer from '../VideoPlayer.svelte';
+</script>
+
+<main>
+    <h1>Video Streaming Example</h1>
+    <VideoPlayer />
+</main>
