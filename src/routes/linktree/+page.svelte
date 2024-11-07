4d96380910f8a053d11c04d5c870015cd1825b99
Joseph Heinz <josephheinz28@gmail.com>
2024-11-07 15:03:22 +0000

    added linktree

---
+++ src/routes/linktree/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Link from "./Link.svelte";
+    const links = [
+        {
+            name: "GitHub",
+            url: "https://github.com/josephheinz",
+            icon: "fa-brands fa-github",
+            color: "bg-rose-500",
+        },
+        {
+            name: "Instagram",
+            url: "https://instagram.com/josephheinz26",
+            icon: "fa-brands fa-instagram",
+            color: "bg-sky-400",
+        },
+        {
+            name: "YouTube",
+            url: "https://youtube.com/@josephheinz",
+            icon: "fa-brands fa-youtube",
+            color: "bg-purple-400",
+        },
+        {
+            name: "Ko-Fi",
+            url: "https://ko-fi.com/josephheinz",
+            icon: "fa-solid fa-mug-hot",
+            color: "bg-orange-400",
+        },
+    ];
+</script>
+
+<main class="flex justify-center items-center bg-[#FADF0B] w-screen h-screen">
+    <div class="p-8 bg-[#F8D6B3] rounded-[27px] gap-4 border-4 border-black shadow-custom flex flex-col justify-evenly items-center">
+        <h1 class="font-black font-sora text-3xl">Joseph Heinz</h1>
+        <br><br>
+        {#each links as link}
+            <Link
+                name={link.name}
+                color={link.color}
+                icon={link.icon}
+                url={link.url}
+            />
+        {/each}
+    </div>
+</main>
---
+++ src/routes/linktree/Link.svelte
@@ -0,0 +1,13 @@
+<style global>
+    @import "../../app.css";
+    @import url("https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Sora:wght@100..800&display=swap");
+    * {box-sizing: border-box;user-select: none;transition: 0.2s ease all;}
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<script>
+    let { name, url, icon, color } = $props();
+</script>
+
+<a class="w-60 {color} py-2 px-4 flex justify-start items-center gap-8 text-lg border-4 border-black shadow-custom rounded-full hover:shadow-none active:shadow-none font-regular font-sora" href="{url}" target="_blank">
+    <i class="{icon} text-3xl"></i> {name}
+</a>
