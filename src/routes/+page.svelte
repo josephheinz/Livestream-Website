5506b0ed3bbe1221d472b43ac0cfd572827a3ced
Joseph Heinz <josephheinz28@gmail.com>
2024-11-07 15:34:02 +0000

    linked to linktree

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,7 +89,7 @@
     <div class="flex flex-col w-80 h-4/6 p-8 bg-[#F8D6B3] border-black border-4 rounded-[27px] justify-evenly items-center shadow-custom aspect-[3/4] gap-4">
       <h1 class="text-5xl font-black font-sora">Socials</h1>
       <img src="Phone_Character.svg" alt="wallet guy" class="h-3/6">
-      <button class="px-6 py-3 bg-[#FF69B4] flex justify-center items-center shadow-custom border-4 border-black rounded-[27px] text-3xl font-sora font-regular active:shadow-none">Linktree</button>
+      <a class="px-6 py-3 bg-[#FF69B4] flex justify-center items-center shadow-custom border-4 border-black rounded-[27px] text-3xl font-sora font-regular active:shadow-none hover:shadow-none" href="/linktree">Linktree</a>
     </div>
   </div>
   <EmptyGridSquare/>
