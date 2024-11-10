0a0c291b009f837c3e2abb5ab1d2e69127738c3b
Qrexx <josephheinz28@gmail.com>
2024-11-09 20:01:54 -0500

    somehow magically fixed it

--- index.js
+++ index.js
@@ -5,35 +5,29 @@ import path from "path";
 import cors from "cors";
 import { handler } from "./build/handler.js";
 import { fileURLToPath } from "url";
-import { disconnect } from "process";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new SocketIOServer(httpServer);
 
-
-app.use(cors());
-
-app.use("/img", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "\\static")));
-app.use("/hls", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "\\hls")));
+let viewers = 0;
 
 io.on('connection', (socket) => {
-	console.log('User connected');
-
 	viewers++;
-	io.emit("viewer-update", viewers);
+  console.log('User connected', viewers);
+	socket.emit("viewer-update", viewers);
 
 	socket.on("disconnect", () => {
-		console.log("User disconnected");
+		console.log("User disconnected", viewers);
 		viewers--;
 		io.emit("viewer-update", viewers);
 	});
 });
 
 app.use(handler);
-app.all('*', (req, res) => {
-    return handler(req, res); // Let SvelteKit handle non-Socket.IO requests
-});
+//app.use(cors());
+app.use("/hls", express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "\\hls")));
+
 
 httpServer.listen(2541, () => {
 	console.log("nodejs server running on localhost:2541");
