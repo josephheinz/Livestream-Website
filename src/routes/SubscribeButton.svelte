81238aceb5abfd3d22d5de25185d55ea888436d9
Qrexx <joheb41@student.oaisd.org>
2024-11-23 10:51:48 -0500

    added link to the sub button

--- src/routes/SubscribeButton.svelte
+++ src/routes/SubscribeButton.svelte
@@ -1,5 +1,7 @@
-<button
+<a 
+  href="https://ko-fi.com/josephheinz/tiers"
+  target="_blank"
   class="px-12 py-2 bg-[#FF69B4] border-4 border-black shadow-custom rounded-full font-oswald font-bold active:shadow-none"
 >
   Subscribe
-</button>
+</a>
